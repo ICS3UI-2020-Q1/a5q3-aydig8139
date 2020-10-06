44ccab2efa45ed29fb04b3e8650045369c0034cc
aydig8139 <aydig8139@wrdsb.ca>
2020-10-06 13:53:11 +0000

    finished question to calculate factorials

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * factorial calculator
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create new scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user for a number and declare a variable to store that number
+    System.out.println("Please enter an integer to calculate the factorial of");
+
+    int userNum = input.nextInt();
+
+    int product = 1;
+
+    for(int multiply = userNum; multiply >= 1; multiply--){
+      product = product*multiply;
+      
+    }
+    System.out.println(userNum + "! = " + product);
   }
 }
